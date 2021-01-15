e1d43f53870bb6b6c823ad5ab24bcd1e4f145667
Jey <jeykang0@gmail.com>
2021-01-15 17:58:17 +0900

    Update wgan_gp_utils.py

--- master_thesis/generative_models/wgan_gp/wgan_gp_utils.py
+++ master_thesis/generative_models/wgan_gp/wgan_gp_utils.py
@@ -1,172 +1,171 @@
 from functools import partial
+
+from keras import Model
+from keras.layers import *
+from keras.layers.merge import _Merge
+from keras.optimizers import Adam
+
 from generative_models import utils
-import tensorflow as tf
 
-"""
-1. Implement Wasserstein loss
-"""
-class Wasserstein(tf.keras.losses.Loss):
-
-  def call(self, y_true, y_pred):
-    y_pred = tf.convert_to_tensor_v2(y_pred)
-    y_true = tf.cast(y_true, y_pred.dtype)
-    return tf.math.reduce_mean(y_true * y_pred, axis=-1)
-
-"""
-2. Implement conv block
-
-Conv1D
-Activation(LeakyReLU)
-MaxPooling1D
-"""
-
-def ConvBlock(x):
-  c = tf.keras.layers.Conv1D(32, 3, padding="same")(x)
-  a = tf.keras.layers.LeakyReLU(alpha=0.2)(c)
-  m = tf.keras.layers.MaxPooling1D(pool_size=2)(a)
-  return m
-
-"""
-3. Implement deconv block
-
-Conv1D
-(Batchnorm)
-Activation(LeakyReLU)
-UpSampling1D
-"""
-
-def DeConvBlock(x):
-  c = tf.keras.layers.Conv1D(32, 3, padding="same")(x)
-  b = tf.keras.layers.BatchNormalization()(c)
-  a = tf.keras.layers.LeakyReLU(alpha=0.2)(b)
-  u = tf.keras.layers.UpSampling1D(size=2)(a)
-  return u
 
 def build_generator(latent_dim, timesteps):
-    
-    gen_input = tf.keras.layers.Input((latent_dim,))
-    gdense0 = tf.keras.layers.Dense(15)(gen_input)
-    bnorm0 = tf.keras.layers.BatchNormalization()(gdense0)
-    gactivation0 = tf.keras.layers.LeakyReLU(alpha=0.2)(bnorm0)
+    generator_inputs = Input((latent_dim,))
+    generated = generator_inputs
 
-    #expand dims before entry into deconv block- something the original paper failed to mention
-    gactivation0 = tf.expand_dims(gactivation0, axis=2)
+    generated = Dense(15)(generated)
+    generated = utils.BatchNormalization()(generated)
+    generated = LeakyReLU(0.2)(generated)
 
-    deconv0 = DeConvBlock(gactivation0)
-    deconv1 = DeConvBlock(deconv0)
-    deconv2 = DeConvBlock(deconv1)
-    gconv0 = tf.keras.layers.Conv1D(1, 3, padding="same")(deconv2)
-    bnorm1 = tf.keras.layers.BatchNormalization()(gconv0)
-    gactivation1 = tf.keras.layers.LeakyReLU(alpha=0.2)(bnorm1)
+    generated = Lambda(lambda x: K.expand_dims(x))(generated)
 
-    #squeeze previously expanded dim now that it's not needed anymore
-    gactivation1 = tf.squeeze(gactivation1, axis=-1)
+    generated = Conv1D(32, 3, padding='same')(generated)
+    generated = utils.BatchNormalization()(generated)
+    generated = LeakyReLU(0.2)(generated)
+    generated = UpSampling1D(2)(generated)
 
-    gdense1 = tf.keras.layers.Dense(timesteps)(gactivation1)
-    activation2 = tf.keras.layers.Activation("tanh")(gdense1)
+    generated = Conv1D(32, 3, padding='same')(generated)
+    generated = utils.BatchNormalization()(generated)
+    generated = LeakyReLU(0.2)(generated)
+    generated = UpSampling1D(2)(generated)
 
-    generator = tf.keras.Model(gen_input, activation2, 'generator')
+    generated = Conv1D(32, 3, padding='same')(generated)
+    generated = utils.BatchNormalization()(generated)
+    generated = LeakyReLU(0.2)(generated)
+    generated = UpSampling1D(2)(generated)
+
+    generated = Conv1D(1, 3, padding='same')(generated)
+    generated = utils.BatchNormalization()(generated)
+    generated = LeakyReLU(0.2)(generated)
+
+    generated = Lambda(lambda x: K.squeeze(x, -1))(generated)
+
+    generated = Dense(timesteps, activation='tanh')(generated)
+
+    generator = Model(generator_inputs, generated, 'generator')
     return generator
 
 
 def build_critic(timesteps, use_mbd, use_packing, packing_degree):
     if use_packing:
-        critic_input = tf.keras.layers.Input((timesteps, packing_degree + 1))
+        critic_inputs = Input((timesteps, packing_degree + 1))
+        criticized = critic_inputs
     else:
-        critic_input = tf.expand_dims(tf.keras.layers.Input((timesteps,)), axis=-1)
-    
-    conv0 = ConvBlock(critic_input)
-    conv1 = ConvBlock(conv0)
-    conv2 = ConvBlock(conv1)
-    cactivation0 = tf.keras.layers.LeakyReLU(alpha=0.2)(conv2)
-    flatten0 = tf.keras.layers.Flatten()(cactivation0)
-    
-    if use_mbd:
-        flatten0 = utils.MinibatchDiscrimination(15, 3)(flatten0)
-    
-    cdense0 = tf.keras.layers.Dense(50)(flatten0)
-    cactivation1 = tf.keras.layers.LeakyReLU(alpha=0.2)(cdense0)
-    cdense1 = tf.keras.layers.Dense(15)(cactivation1)
-    cactivation2 = tf.keras.layers.LeakyReLU(alpha=0.2)(cdense1)
-    cdense2 = tf.keras.layers.Dense(1)(cactivation2)
+        critic_inputs = Input((timesteps,))
+        criticized = Lambda(lambda x: K.expand_dims(x, -1))(critic_inputs)
 
-    critic = tf.keras.Model(critic_input, cdense2, 'critic')
+    criticized = Conv1D(32, 3, padding='same')(criticized)
+    criticized = LeakyReLU(0.2)(criticized)
+    criticized = MaxPooling1D(2, padding='same')(criticized)
+
+    criticized = Conv1D(32, 3, padding='same')(criticized)
+    criticized = LeakyReLU(0.2)(criticized)
+    criticized = MaxPooling1D(2, padding='same')(criticized)
+
+    criticized = Conv1D(32, 3, padding='same')(criticized)
+    criticized = LeakyReLU(0.2)(criticized)
+    criticized = MaxPooling1D(2, padding='same')(criticized)
+
+    criticized = Conv1D(32, 3, padding='same')(criticized)
+    criticized = LeakyReLU(0.2)(criticized)
+
+    criticized = Flatten()(criticized)
+    if use_mbd:
+        criticized = utils.MinibatchDiscrimination(15, 3)(criticized)
+
+    criticized = Dense(50)(criticized)
+    criticized = LeakyReLU(0.2)(criticized)
+    criticized = Dense(15)(criticized)
+    criticized = LeakyReLU(0.2)(criticized)
+    criticized = Dense(1)(criticized)
+
+    critic = Model(critic_inputs, criticized, 'critic')
 
     return critic
 
 
 def build_generator_model(generator, critic, latent_dim, timesteps, use_packing, packing_degree, batch_size,
                           generator_lr):
-    generator.trainable = True
-    critic.trainable = False
+    utils.set_model_trainable(generator, True)
+    utils.set_model_trainable(critic, False)
 
-    noise_samples = tf.keras.layers.Input((latent_dim,))
+    noise_samples = Input((latent_dim,))
     generated_samples = generator(noise_samples)
 
     if use_packing:
-        generated_samples = tf.reshape(generated_samples, (batch_size, timesteps, 1))
-        supporting_noise_samples = tf.keras.layers.Input((latent_dim, packing_degree))
-        
-        reshaped_supporting_noise_samples = tf.reshape(supporting_noise_samples, (batch_size * packing_degree, latent_dim))
+        generated_samples = Lambda(lambda x: K.reshape(x, (batch_size, timesteps, 1)))(generated_samples)
+        supporting_noise_samples = Input((latent_dim, packing_degree))
+
+        reshaped_supporting_noise_samples = Lambda(
+            lambda x: K.reshape(x, (batch_size * packing_degree, latent_dim)))(
+            supporting_noise_samples)
 
         supporting_generated_samples = generator(reshaped_supporting_noise_samples)
-        
-        supporting_generated_samples = tf.reshape(supporting_generated_samples, (batch_size, timesteps, packing_degree))
-        
-        merged_generated_samples = tf.keras.layers.Concatenate(axis=-1)([generated_samples, supporting_generated_samples])
+        supporting_generated_samples = Lambda(
+            lambda x: K.reshape(x, (batch_size, timesteps, packing_degree)))(
+            supporting_generated_samples)
+        merged_generated_samples = Concatenate(-1)([generated_samples, supporting_generated_samples])
 
         generated_criticized = critic(merged_generated_samples)
 
-        generator_model = tf.keras.Model([noise_samples, supporting_noise_samples], generated_criticized, name='generator_model')
-        generator_model.compile(optimizer=tf.keras.optimizers.Adam(generator_lr, beta_1=0, beta_2=0.9), loss=utils.wasserstein_loss)
+        generator_model = Model([noise_samples, supporting_noise_samples], generated_criticized, 'generator_model')
+        generator_model.compile(optimizer=Adam(generator_lr, beta_1=0, beta_2=0.9), loss=utils.wasserstein_loss)
     else:
         generated_criticized = critic(generated_samples)
 
-        generator_model = tf.keras.Model([noise_samples], generated_criticized, name='generator_model')
-        generator_model.compile(optimizer=tf.keras.optimizers.Adam(generator_lr, beta_1=0, beta_2=0.9), loss=utils.wasserstein_loss)
+        generator_model = Model([noise_samples], generated_criticized, 'generator_model')
+        generator_model.compile(optimizer=Adam(generator_lr, beta_1=0, beta_2=0.9), loss=utils.wasserstein_loss)
     return generator_model
 
 
 def build_critic_model(generator, critic, latent_dim, timesteps, use_packing, packing_degree, batch_size, critic_lr,
                        gradient_penality_weight):
-    generator.trainable = False
-    critic.trainable = True
+    utils.set_model_trainable(generator, False)
+    utils.set_model_trainable(critic, True)
 
-    noise_samples = tf.keras.layers.Input((latent_dim,))
-    real_samples = tf.keras.layers.Input((timesteps,))
+    noise_samples = Input((latent_dim,))
+    real_samples = Input((timesteps,))
 
     if use_packing:
-        supporting_noise_samples = tf.keras.layers.Input((latent_dim, packing_degree))
-        supporting_real_samples = tf.keras.layers.Input((timesteps, packing_degree))
+        supporting_noise_samples = Input((latent_dim, packing_degree))
+        supporting_real_samples = Input((timesteps, packing_degree))
 
-        reshaped_supporting_noise_samples = tf.reshape(supporting_noise_samples, (batch_size * packing_degree, latent_dim))
-        
+        reshaped_supporting_noise_samples = Lambda(
+            lambda x: K.reshape(x, (batch_size * packing_degree, latent_dim)))(
+            supporting_noise_samples)
         generated_samples = generator(noise_samples)
         supporting_generated_samples = generator(reshaped_supporting_noise_samples)
-        
-        expanded_generated_samples = tf.reshape(generated_samples, (batch_size, timesteps, 1))
-        expanded_generated_supporting_samples = tf.reshape(supporting_generated_samples, (batch_size, timesteps, packing_degree))
 
-        merged_generated_samples = tf.keras.layers.Concatenate(axis=-1)([expanded_generated_samples, expanded_generated_supporting_samples])
+        expanded_generated_samples = Lambda(lambda x: K.reshape(x, (batch_size, timesteps, 1)))(
+            generated_samples)
+        expanded_generated_supporting_samples = Lambda(
+            lambda x: K.reshape(x, (batch_size, timesteps, packing_degree)))(
+            supporting_generated_samples)
+
+        merged_generated_samples = Concatenate(-1)([expanded_generated_samples, expanded_generated_supporting_samples])
 
         generated_criticized = critic(merged_generated_samples)
-        
-        expanded_real_samples = tf.reshape(real_samples, (batch_size, timesteps, 1))
-        merged_real_samples = tf.concat([expanded_real_samples, supporting_real_samples], -1)
+
+        expanded_real_samples = Lambda(lambda x: K.reshape(x, (batch_size, timesteps, 1)))(real_samples)
+        merged_real_samples = Lambda(lambda x: K.concatenate(x, -1))([expanded_real_samples, supporting_real_samples])
 
         real_criticized = critic(merged_real_samples)
 
         averaged_samples = RandomWeightedAverage(batch_size)([real_samples, generated_samples])
-        
-        expanded_averaged_samples = tf.reshape(averaged_samples, (batch_size, timesteps, 1))
-        
-        expanded_supporting_real_samples = tf.reshape(supporting_real_samples, (batch_size * packing_degree, timesteps))
+
+        expanded_averaged_samples = Lambda(lambda x: K.reshape(x, (batch_size, timesteps, 1)))(
+            averaged_samples)
+
+        expanded_supporting_real_samples = Lambda(
+            lambda x: K.reshape(x, ((batch_size * packing_degree), timesteps)))(
+            supporting_real_samples)
         averaged_support_samples = RandomWeightedAverage((batch_size * packing_degree))(
             [expanded_supporting_real_samples, supporting_generated_samples])
-        
-        averaged_support_samples = tf.reshape(averaged_support_samples, (batch_size, timesteps, packing_degree))
 
-        merged_averaged_samples = tf.keras.layers.Concatenate(axis=-1)([expanded_averaged_samples, averaged_support_samples])
+        averaged_support_samples = Lambda(
+            lambda x: K.reshape(x, (batch_size, timesteps, packing_degree)))(
+            averaged_support_samples)
+
+        merged_averaged_samples = Concatenate(-1)([expanded_averaged_samples, averaged_support_samples])
 
         averaged_criticized = critic(merged_averaged_samples)
 
@@ -175,10 +174,10 @@ def build_critic_model(generator, critic, latent_dim, timesteps, use_packing, pa
                                   gradient_penalty_weight=gradient_penality_weight)
         partial_gp_loss.__name__ = 'gradient_penalty'
 
-        critic_model = tf.keras.Model([real_samples, noise_samples, supporting_real_samples, supporting_noise_samples],
-                             [real_criticized, generated_criticized, averaged_criticized], name='critic_model')
+        critic_model = Model([real_samples, noise_samples, supporting_real_samples, supporting_noise_samples],
+                             [real_criticized, generated_criticized, averaged_criticized], 'critic_model')
 
-        critic_model.compile(optimizer=tf.keras.optimizers.Adam(critic_lr, beta_1=0, beta_2=0.9),
+        critic_model.compile(optimizer=Adam(critic_lr, beta_1=0, beta_2=0.9),
                              loss=[utils.wasserstein_loss, utils.wasserstein_loss, partial_gp_loss],
                              loss_weights=[1 / 3, 1 / 3, 1 / 3])
     else:
@@ -204,20 +203,20 @@ def build_critic_model(generator, critic, latent_dim, timesteps, use_packing, pa
 
 
 def gradient_penalty_loss(_, y_pred, averaged_samples, gradient_penalty_weight):
-    gradients = tf.gradients(y_pred, averaged_samples)[0]
-    gradients_sqr = tf.math.square(gradients)
-    gradients_sqr_sum = tf.math.reduce_sum(gradients_sqr, axis=np.arange(1, len(gradients_sqr.shape)))
-    gradient_l2_norm = tf.math.sqrt(gradients_sqr_sum)
-    gradient_penalty = gradient_penalty_weight * tf.math.square(1 - gradient_l2_norm)
-    return tf.math.reduce_mean(gradient_penalty)
+    gradients = K.gradients(y_pred, averaged_samples)[0]
+    gradients_sqr = K.square(gradients)
+    gradients_sqr_sum = K.sum(gradients_sqr, axis=np.arange(1, len(gradients_sqr.shape)))
+    gradient_l2_norm = K.sqrt(gradients_sqr_sum)
+    gradient_penalty = gradient_penalty_weight * K.square(1 - gradient_l2_norm)
+    return K.mean(gradient_penalty)
 
 
-class RandomWeightedAverage(tf.keras.layers.Layer):
+class RandomWeightedAverage(_Merge):
     def __init__(self, batch_size, **kwargs):
         super().__init__(**kwargs)
         self._batch_size = batch_size
 
     def _merge_function(self, inputs):
-        weights = tf.random.uniform((self._batch_size, 1))
+        weights = K.random_uniform((self._batch_size, 1))
         averaged_inputs = (weights * inputs[0]) + ((1 - weights) * inputs[1])
         return averaged_inputs
